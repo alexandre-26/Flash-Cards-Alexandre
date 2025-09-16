92d9e4105e1673f7ab08f65d4f758bc868717560
alexandre-26 <alexandre.fraron.campos@escola.pr.gov.br>
2025-09-16 09:44:19 -0300

    16/09

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,52 @@
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Conhecimetos gerais',
+    'Qual é o maior continente do mundo?',
+    'Àsia'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Quem foi o autor da obra "O Príncipe"?',
+    'Nicolau Maquiavel'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Em que ano aconteceu a Independência do Brasil?',
+    '1822'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Qual é o maior planeta do Sistema Solar?',
+    'Júpiter'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Qual é a capital da França?',
+    'Paris'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Em que país nasceu Albert Einstein?',
+    'Alemanha'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Qual é o símbolo químico da água?',
+    'H₂O'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Qual é o maior animal terrestre?',
+    'Elefante africano'
+)
+criaCartao(
+    'Conhecimetos gerais',
+    'Quem escreveu a peça "Romeu e Julieta"?',
+    'William Shakespeare'
 )
 
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
 
