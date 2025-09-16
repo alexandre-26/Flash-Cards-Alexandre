08fc4188ef2d74ebab38ab05eeb29b3bdb0876b2
alexandre-26 <alexandre.fraron.campos@escola.pr.gov.br>
2025-09-16 10:16:34 -0300

    16/09

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-kkkkk
+function criaCartao(categoria,pergunta,resposta){
+    let container = document.getElementById('container')
+    let cartao= document. createElement('article')
+
+
+
+
+
+
+    
+}
